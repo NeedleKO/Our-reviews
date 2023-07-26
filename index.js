332d028c4a17afa323fe3d103c2b66cd1fc96dfc
NeedleKO <bootscats@inbox.lv>
2023-07-26 13:56:12 +0300

    finished reviews section

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// const personImg = document.getElementById("person-img");
-// const workerImg1 = (document.getElementById("person-img").src =
-//   "/workers imgs/worker1.jpg");
-// const workerImg2 = document.getElementById("person-img").src = "/workers imgs/worker2.jpg";
-
 const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
@@ -10,6 +5,8 @@ const info = document.getElementById("info");
 const randomBtn = document.querySelector(".randomBtn");
 const rightBtn = document.querySelector(".rightBtn");
 const leftBtn = document.querySelector(".leftBtn");
+
+
 
 const reviews = [
   {
@@ -29,6 +26,9 @@ const reviews = [
     job: "THE PHOTOGRAPHER",
   },
 ];
+
+// let randomGenerated = Math.floor(Math.random() * reviews.length);
+// console.log(randomGenerated);
 
 let counter = 0;
 
@@ -88,3 +88,29 @@ leftBtn.addEventListener("click", () => {
     document.getElementById("person-img").src = "/workers imgs/worker3.jpg";
   }
 });
+//random btn logic
+randomBtn.addEventListener("click", () => {
+  let randomGenerated = Math.floor(Math.random() * reviews.length);
+  console.log(randomGenerated);
+  if (reviews[randomGenerated].id === 1) {
+    author.textContent = reviews[randomGenerated].name;
+    job.textContent = reviews[randomGenerated].job;
+    info.textContent =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consequatur, iure perspiciatis illo sunt placeat similique labore ab doloribus ratione quis voluptas non voluptatum vero blanditiis delectus dolore in tenetur.";
+    document.getElementById("person-img").src = "/workers imgs/worker1.jpg";
+  }
+  if (reviews[randomGenerated].id === 2) {
+    author.textContent = reviews[randomGenerated].name;
+    job.textContent = reviews[randomGenerated].job;
+    info.textContent =
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores nobis enim ut unde perspiciatis, dolores eos fugiat mollitia sint at nisi culpa molestias illo dolor, quos, voluptatibus a aut alias.";
+    document.getElementById("person-img").src = "/workers imgs/worker2.jpg";
+  }
+  if (reviews[randomGenerated].id === 3) {
+    author.textContent = reviews[randomGenerated].name;
+    job.textContent = reviews[randomGenerated].job;
+    info.textContent =
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos libero excepturi distinctio odio iure? Natus ipsum, iure fugit rem similique optio alias porro distinctio reprehenderit.";
+    document.getElementById("person-img").src = "/workers imgs/worker3.jpg";
+  }
+})
