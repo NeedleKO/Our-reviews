c04f13fd9e185e7f51db5abc23e46b23bd8f82a7
NeedleKO <bootscats@inbox.lv>
2023-07-26 12:49:38 +0300

    finished review section

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 const author = document.getElementById("author");
 const job = document.getElementById("job");
+const info = document.getElementById("info");
 
 const randomBtn = document.querySelector(".randomBtn");
 const rightBtn = document.querySelector(".rightBtn");
@@ -40,22 +41,24 @@ rightBtn.addEventListener("click", () => {
   if (reviews[counter].id === 1) {
     author.textContent = reviews[counter].name;
     job.textContent = reviews[counter].job;
+    info.textContent =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consequatur, iure perspiciatis illo sunt placeat similique labore ab doloribus ratione quis voluptas non voluptatum vero blanditiis delectus dolore in tenetur.";
     document.getElementById("person-img").src = "/workers imgs/worker1.jpg";
   }
   if (reviews[counter].id === 2) {
     author.textContent = reviews[counter].name;
     job.textContent = reviews[counter].job;
+    info.textContent =
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores nobis enim ut unde perspiciatis, dolores eos fugiat mollitia sint at nisi culpa molestias illo dolor, quos, voluptatibus a aut alias.";
     document.getElementById("person-img").src = "/workers imgs/worker2.jpg";
   }
   if (reviews[counter].id === 3) {
     author.textContent = reviews[counter].name;
     job.textContent = reviews[counter].job;
+    info.textContent =
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos libero excepturi distinctio odio iure? Natus ipsum, iure fugit rem similique optio alias porro distinctio reprehenderit.";
     document.getElementById("person-img").src = "/workers imgs/worker3.jpg";
   }
-
-  console.log(counter);
-
-  
 });
 
 leftBtn.addEventListener("click", () => {
@@ -66,17 +69,22 @@ leftBtn.addEventListener("click", () => {
   if (reviews[counter].id === 1) {
     author.textContent = reviews[counter].name;
     job.textContent = reviews[counter].job;
+    info.textContent =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consequatur, iure perspiciatis illo sunt placeat similique labore ab doloribus ratione quis voluptas non voluptatum vero blanditiis delectus dolore in tenetur.";
     document.getElementById("person-img").src = "/workers imgs/worker1.jpg";
   }
   if (reviews[counter].id === 2) {
     author.textContent = reviews[counter].name;
     job.textContent = reviews[counter].job;
+    info.textContent =
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores nobis enim ut unde perspiciatis, dolores eos fugiat mollitia sint at nisi culpa molestias illo dolor, quos, voluptatibus a aut alias.";
     document.getElementById("person-img").src = "/workers imgs/worker2.jpg";
   }
   if (reviews[counter].id === 3) {
     author.textContent = reviews[counter].name;
     job.textContent = reviews[counter].job;
+    info.textContent =
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos libero excepturi distinctio odio iure? Natus ipsum, iure fugit rem similique optio alias porro distinctio reprehenderit.";
     document.getElementById("person-img").src = "/workers imgs/worker3.jpg";
   }
-  console.log(counter);
 });
